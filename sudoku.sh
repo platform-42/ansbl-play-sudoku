eb72d5cc449e697ea2b95c7213f5b2cd7a6086b9
Diederick de Buck <diederickdebuck@liberator.home>
2025-11-12 12:41:28 +0100

    ddd

--- sudoku.sh
+++ sudoku.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
-BRANCH=development
+BRANCH=main
 cd ${SCRIPTPATH}
 
 ansible-galaxy collection install git@github.com:platform-42/platform42.neo4j.git,${BRANCH}
