6e0cf5b9c80a49aae319c0b010792c8ef4077459
Diederick de Buck <diederickdebuck@liberator.home>
2025-11-26 22:17:15 +0100

    ddd

---
+++ vars/vertices/bulk/generate.py
@@ -0,0 +1,42 @@
+import yaml
+
+cells = []
+
+for row in range(1, 10):
+    for col in range(1, 10):
+        cell = {
+            "label": "Cell",
+            "entity_name": f"C{row}{col}",
+            "properties": {
+                "row": {
+                    "value": row,
+                    "type": "int"
+                },
+                "column": {
+                    "value": col,
+                    "type": "int"
+                },
+                "domain": {
+                    "value": [1,2,3,4,5,6,7,8,9],
+                    "type": "list",
+                    "element_type": "int"
+                },
+                "color": {
+                    "value": 0,
+                    "type": "int"
+                },
+                "color_backup": {
+                    "value": 0,
+                    "type": "int"
+                },
+                "domain_backup": {
+                    "value": [],
+                    "type": "list",
+                    "element_type": "int"
+                }
+            }
+        }
+        cells.append(cell)
+
+# Dump YAML
+print(yaml.dump(cells, sort_keys=False))
